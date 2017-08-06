f81824650e468e22f41f42e27f1cae9f53a2c315
misMacBookPro <mis@misMacBookPro-2.local>
2017-08-06 13:23:23 +0200

    added the model

---
+++ src/app/models/rouiter.model.ts
@@ -0,0 +1,12 @@
+
+export interface rouiter  {
+  Id: string; ///Id
+  name: string;
+  last_trade: number;
+  net_change: number;
+  percentage_change: number;
+
+  trade_list?: number[];
+  bids_asks?: number[];
+  chart?: number[];
+}

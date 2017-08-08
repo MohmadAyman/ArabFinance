46a10bdc201dfe3fb2f9f005a83cdb2420bd7248
ahmedabdalmageed95 <ahmedabdalmageed95@yahoo.com>
2017-08-08 16:10:18 +0200

    merged

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+// import {DataSource} from '@angular/cdk';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-market',
@@ -6,6 +12,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
+  displayedColumns = ['userId', 'userName', 'progress', 'color'];
+  // exampleDatabase = new ExampleDatabase();
+  // dataSource: ExampleDataSource | null;
+
   constructor() { }
   ngOnInit() {
     function top_5() {

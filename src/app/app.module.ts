153694d8a94e8ce78ad00ece08d45b25c6fffaac
misMacBookPro <mis@misMacBookPro-2.local>
2017-08-08 16:07:06 +0200

    tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import {Http, HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login.component';
-import {MarketComponent} from './market.component';
+import {MarketComponent} from './market/market.component';
 import {MoreComponent} from './more.component';
 import {WatchlistComponent} from './watchlist.component';
 import {NewsComponent} from './news.component';
 import {PagenotfoundComponent} from './pagenotfound.component';
 import {StockService} from './stock.service';
 import {Angular2TokenService , A2tUiModule } from 'angular2-token';
-const approutes: Routes = [{path: 'Home', component: AppComponent},
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdTabsModule} from '@angular/material';
+
+const approutes: Routes = [{path: '', component: LoginComponent},
 { path: 'Watchlist', component: WatchlistComponent } ,
 { path: 'News', component: NewsComponent} ,
 { path: 'Market', component: MarketComponent} ,
@@ -24,7 +28,7 @@ const approutes: Routes = [{path: 'Home', component: AppComponent},
     AppComponent , LoginComponent , MarketComponent , MoreComponent , WatchlistComponent , NewsComponent , PagenotfoundComponent
   ],
   imports: [
-   BrowserModule , FormsModule , HttpModule , RouterModule.forRoot(approutes) , A2tUiModule
+    MdTabsModule, BrowserModule , FormsModule , HttpModule , BrowserAnimationsModule , RouterModule.forRoot(approutes) , A2tUiModule
   ],
   providers: [StockService , Angular2TokenService],
   bootstrap: [AppComponent]
